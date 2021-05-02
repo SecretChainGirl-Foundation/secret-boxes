7799eb0e80445c422d4018e5cbb91386c2449e07
unknown <brave.tiger1227@gmail.com>
2021-05-02 14:04:54 -0400

    tutorial

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from "../pages/Index.vue";
-import Tutorial from "../pages/Tutorial.vue";
 import SecretCounter from "../pages/SecretCounter.vue";
+import Tutorial from "../pages/Tutorial.vue";
+import TutorialLaunching from "../pages/TutorialLaunching.vue";
+import Support from "../pages/Support.vue";
+import ExampleComponent from "../pages/ExampleComponent.vue";
 
 const routes = [
   {
@@ -10,20 +13,39 @@ const routes = [
     component: Home,
   },
   {
+    path: "/secret/counter",
+    name: "SecretCounter",
+    component: SecretCounter,
+  },
+  {
     path: "/tutorial",
     name: "Tutorial",
     component: Tutorial,
   },
   {
-    path: "/secret-counter",
-    name: "SecretCounter",
-    component: SecretCounter,
+    path: "/tutorial/launching",
+    name: "TutorialLaunching",
+    component: TutorialLaunching,
   },
+  {
+    path: "/support",
+    name: "Support",
+    component: Support,
+  },
+  {
+    path: "/example-component",
+    name: "ExampleComponent",
+    component: ExampleComponent
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    // always scroll to top
+    return { top: 0 }
+  },
 });
 
 export default router;
